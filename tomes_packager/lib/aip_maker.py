f24666a5c80eb1058794dac3bf96d58859f96561
Nitin Arora <nitaro74@gmail.com>
2018-05-09 16:44:15 -0400

    Added reusable methods.

--- tomes_packager/lib/aip_maker.py
+++ tomes_packager/lib/aip_maker.py
@@ -4,11 +4,13 @@
 TOMES archival information package (AIP).
 
 Todo:
-    * @file_exclusions: going to actually use this?
+    * @file_exclusions: going to actually us this?
     * Check in-line TODOs.
     * Need to move extraneous files to metadata/.
         - But how to identify extraneous? By basenames?
-    * Add logging for positive results, too. Be happy!
+    * Have a cleanup function to clean up the hot folder.
+    * Make a general purpose file mover function AND one for folders.
+    * Vadlidate AIP after the fact - or during.
 """
 
 # import modules.
@@ -39,18 +41,12 @@ class AIPMaker():
             - TypeError: If @files_exclusions is not a list.
         """
 
-        # set logger; suppress logging by default.
-        self.logger = logging.getLogger(__name__)
-        self.logger.addHandler(logging.NullHandler())
-        
-        # verify @source_dir and @destination_dir are folders.
+        # verify source_dir and destination_dir are folders.
         if not os.path.isdir(source_dir):
             msg = "Can't find source: {}".format(source_dir)
-            self.logger.error(msg)
             raise NotADirectoryError(msg)
         if not os.path.isdir(destination_dir):
             msg = "Can't find destination: {}".format(destination_dir)
-            self.logger.error(msg)
             raise NotADirectoryError(msg)
 
         # verify @files_exclusions is a list:
@@ -59,28 +55,34 @@ class AIPMaker():
                     type(files_exclusions))
             raise TypeError(msg)
         
+        # set logger; suppress logging by default.
+        self.logger = logging.getLogger(__name__)
+        self.logger.addHandler(logging.NullHandler())
+
         # set attributes.
         self.account_id = str(account_id) 
         self.source_dir = source_dir
         self.destination_dir = destination_dir
         self.file_exclusions = file_exclusions
         
-        # convenience functions to normalize and join paths.
+        # convenience functions to join paths and normalize them.
         self._normalize_path = lambda p: os.path.normpath(p).replace("\\", "/")  
         self._join_paths = lambda *l: self._normalize_path(os.path.join(*l))
         
-        # set additional attributes.
-        self.data = glob.glob(self._join_paths(self.source_dir, "**"), recursive=True)
-        self.folders = [f for f in self.data if os.path.isdir(f)]
-        self.files = [f for f in self.data if os.path.isfile(f)]
+        # set source attributes.
+        self.source_data = glob.glob(self._join_paths(self.source_dir, "**"), recursive=True)
+        self.source_folders = [f for f in self.source_data if os.path.isdir(f)]
+        self.source_files = [f for f in self.source_data if os.path.isfile(f)]
+        self.source_eaxs = self._join_paths(self.source_dir, "eaxs", self.account_id)
+        self.source_mime = self._join_paths(self.source_dir, "mime", self.account_id)
+        self.source_pst = self._join_paths(self.source_dir, "pst")
+
+        # set destination attributes.
         self.root = self._make_dir()
         self.metadata_dir = self._make_dir("metadata")
         self.eaxs_dir = self._make_dir("eaxs")
         self.mime_dir = self._make_dir("mime")
         self.pst_dir = self._make_dir("pst")
-
-        # create AIP.
-        self.make_aip()
 
 
     def _make_dir(self, subdir=None):
@@ -115,56 +117,64 @@ class AIPMaker():
         """ ???
 
         Returns:
-            ???
+            None
         """
 
-        if self.root == "":
+        if not os.path.isdir(self.root):
             self.logger.warning("Cannot add files to non-existant AIP folder; aborting.")
-            return None
-        
-        # move optional PST file.
-        pst_file = self.account_id + ".pst"
-        psts = [os.path.abspath(f) for f in self.files if os.path.basename(f) == pst_file]
-        
-        if len(psts) == 0:
-            self.logger.info("NO .pst file fount, removing: {}".format(self.pst_folder))
-            os.rmdir(self.pst_folder)
-            # TODO: OK, PST not required. ???
-        else:
-            pst = psts[0]
-            try:
-                shutil.move(pst, self.pst_dir)
-            except Exception as err:
-                # TODO: What specific exceptions?
-                self.logger.warning("???: {}".format(pst))
-                self.logger.error(err)
-                # TODO: ???
+            return
 
-        # move required EAXS files.
-        source_eaxs = self._join_paths(self.source_dir, "eaxs", self.account_id)
+        self._move_files(self.source_pst, self.pst_dir, delete_empty=True)
+        self._move_subdirs(self.source_eaxs, self.eaxs_dir)
+        self._move_subdirs(self.source_mime, self.mime_dir)
+
+        return
+
+
+    def _move_files(self, source, destination, required=False, delete_empty=False):
+        """ """
         
-        if not os.path.isdir(source_eaxs):
-            self.logger.warning("???")
-            self.logger.error("???")
-            # TODO: raise something?
-        else:
+        # ???
+        files = [self._normalize_path(f) for f in self.source_files
+                 if os.path.splitext(os.path.basename(f))[0] == self.account_id
+                 and self._normalize_path(os.path.dirname(f)) == source]
+        if len(files) == 0:
+            self.logger.info("???")
+            if delete_empty:
+                self.logger.info("???")
+                os.rmdir(destination)
+            # TODO: OK, PST not required. ???
+            return
+
+        # ???
+        for fil in files:
             try:
-                shutil.move(source_eaxs, self.eaxs_dir)
+                shutil.move(fil, destination)
             except Exception as err:
                 # TODO: What specific exceptions?
                 self.logger.warning("???: {}".format())
                 self.logger.error(err)
                 # TODO: ???
+
+        return
+
+
+    def _move_subdirs(self, source, destination, required=False):
+        """ Moves first level subdirectories and their files ... """
         
-        # move MIME files.
-        source_mime = self._join_paths(self.source_dir, "mime", self.account_id)
-        if not os.path.isdir(source_mime):
+        # ???       
+        if not os.path.isdir(source):
             self.logger.warning("???")
             self.logger.error("???")
             # TODO: raise something?
-        else:
+            return
+
+        # ???
+        subfolders = [self._normalize_path(f) for f in self.source_folders
+                      if self._normalize_path(os.path.dirname(f)) == source]
+        for subfolder in subfolders:
             try:
-                shutil.move(source_mime, self.mime_dir)
+                shutil.move(subfolder, destination)
             except Exception as err:
                 # TODO: What specific exceptions?
                 self.logger.warning("???: {}".format())
@@ -180,3 +190,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     f = "../../tests/sample_files"
     am = AIPMaker("foo", f+"/hot_folder", f)
+    am.make_aip()
+    am = AIPMaker("bar", f+"/hot_folder", f)
+    am.make_aip()
+
