1d4d3c617100fff7a0e652c395d0debb0dd2ab67
Nitaro <nitaro74@gmail.com>
2018-06-04 12:52:09 -0400

    Added AIP test.

--- tests/test__aip_maker.py
+++ tests/test__aip_maker.py
@@ -4,52 +4,96 @@
 import sys; sys.path.append("..")
 import unittest
 import logging
+import os
+import random
+import shutil
 import tempfile
 from datetime import datetime
-from tomes_packager.lib.mets_maker import *
+from zipfile import ZipFile
+from tomes_packager.lib.aip_maker import *
 
 # enable logging.
 logging.basicConfig(level=logging.DEBUG)
 
+# set variables.
+ACCOUNTS = ["foo", "bar"]
+SAMPLE_FOLDER = os.path.join(os.path.dirname(__file__), "sample_files")
+HOT_FILE = os.path.join(SAMPLE_FOLDER, "hot_folder.zip")
+HOT_FOLDER = os.path.join(SAMPLE_FOLDER, "hot_folder")
 
-class Test_MetsMaker(unittest.TestCase):
+# deletes a folder and re-unzips @HOT_FILE into @SAMPLE_FOLDER.
+def cleanup(folder):
 
+    # remove @folder.
+    try:
+        shutil.rmtree(folder)
+    except Exception as err:
+        logging.warning("Can't delete test folder: {}".format(folder))
+        logging.error(err)
+
+    # unzip @HOT_FILE to @SAMPLE_FOLDER.
+    logging.info("Deleting AIP: {}".format(folder))
+    with ZipFile(HOT_FILE) as zf:
+        try:
+            zf.extractall(SAMPLE_FOLDER)
+        except Exception as err:
+            logging.warning("Can't unzip: {}".format(HOT_FILE))
+            logging.error(err)
     
+    return
+
+
+class Test_AIPMaker(unittest.TestCase):
+
+
     def setUp(self):
 
         # set attributes.
-        self.sample_file = "sample_files/sample_mets_template.xml"
+        self.sample_folder = SAMPLE_FOLDER
+        self.hot_file = HOT_FILE
+        self.hot_folder = HOT_FOLDER
+        self.accounts = ACCOUNTS
+        self.cleanup = cleanup
 
 
-    def test__mets_validity(self):
-        """ Is a rendered METS valid? """
+    def test__aip_validity(self):
+        """ Is a rendered AIP valid? """
         
-        # make temporary file, save the filename, then delete the file.
-        mets_handle, mets_path = tempfile.mkstemp(dir=".", suffix=".xml")
-        os.close(mets_handle)
-        os.remove(mets_path)
+        # pick a ranndom account.
+        account = random.choice(self.accounts)
 
-        # write a temporary METS file.
-        self.mm = METSMaker(self.sample_file, mets_path,
-                TIMESTAMP = lambda: datetime.now().isoformat() + "Z")
-        self.mm.make()
+        # make the AIP.
+        am = AIPMaker(account, self.hot_folder, self.sample_folder)    
+        am.make()
 
-        # see if METS is valid.
-        self.assertTrue(self.mm.validate())
-        os.remove(mets_path)
+        # see if AIP is valid.
+        self.assertTrue(am.validate())
+        self.cleanup(am.root)
 
 
 # CLI.
-def main(template: "METS template file", output_file: "output METS XML file"):
+def main(account_id:("email account identifier", "positional", None, str, ACCOUNTS), 
+        delete_aip:("delete the AIP and reset sample_files/hot_folder", "flag", "d")=False):
     
-    "Renders METS template with callable \"TIMESTAMP()\" variable to a METS XML file.\
-    \nexample: `py -3 test__mets_maker.py sample_files/sample_mets_template.xml out.xml`"
+    "Creates AIP folder from data in source directory \"sample_files/hot_folder\".\
+    \nexample: `py -3 test__aip_maker.py foo`"
 
-    # create and self-validate METS file.
-    mm = METSMaker(template, output_file, 
-            TIMESTAMP = lambda: datetime.now().isoformat() + "Z")
-    mm.make()
-    mm.validate()
+    # create and self-validate an AIP.
+    try:
+        am = AIPMaker(account_id, "sample_files/hot_folder", "sample_files/")    
+        am.make()
+        is_valid = am.validate()
+    except Exception as err:
+        is_valid = False
+        logging.warning("Can't make AIP for: {}".format(account_id))
+        logging.error(err)
+
+    # if needed, run @cleanup().
+    if delete_aip:
+        cleanup(am.root)
+
+    # print if the AIP is/was valid.
+    print(is_valid)
 
 
 if __name__ == "__main__":
